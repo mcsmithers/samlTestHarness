09858b7a13c1c88c39d14f68d21056535039d399
Christina Smithers <christina.smithers@gmail.com>
2019-10-14 13:25:57 -0500

    components written out and ready to start customizing

--- test-harness/apps/test-harness/src/app/app.module.ts
+++ test-harness/apps/test-harness/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { LoansModule } from './loans/loans.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoansComponent } from './loans/loans.component';
 
 @NgModule({
-  declarations: [AppComponent, LoansComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, HomeModule, AccountsModule, LoansModule],
   providers: [],
   bootstrap: [AppComponent]
 })
